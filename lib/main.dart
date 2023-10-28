b1721ae968f78a2a5fe1da03d462362a983e3e32
IsuruUpeka <upekaisuru@gmail.com>
2023-10-28 14:47:36 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_chat/common/routes/pages.dart';
+import 'package:firebase_chat/common/services/services.dart';
+import 'package:firebase_chat/common/store/config.dart';
+import 'package:firebase_chat/common/store/store.dart';
+import 'package:firebase_chat/firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Get.putAsync<StorageService>(()=>StorageService().init());
+  Get.put<ConfigStore>(ConfigStore());
+  Get.put<UserStore>(UserStore());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (BuildContext context, Widget? child)=>
+          GetMaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+
+              primarySwatch: Colors.blue,
+            ),
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+            //home: Center(child: Container(child: Text("Project started"))),
+          ),
+
+    );
+  }
+}
---
+++ lib/pages/sign_in/controller.dart
@@ -0,0 +1,76 @@
+import 'package:firebase_chat/common/entities/entities.dart';
+import 'package:firebase_chat/common/store/store.dart';
+import 'package:firebase_chat/common/widgets/toast.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../common/routes/names.dart';
+import 'index.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes:<String>[
+    'openid'
+  ]
+);
+
+class SignInController extends GetxController{
+  final state = SignInState();
+  SignInController();
+  final db = FirebaseFirestore.instance;
+  Future <void> handleSignIn() async {
+    try{
+      var user = await _googleSignIn.signIn();
+      if(user!=null){
+        String displayName = user.displayName??user.email;
+        String email = user.email;
+        String id = user.id;
+        String photoUrl = user.photoUrl??"";
+        UserLoginResponseEntity userProfile = UserLoginResponseEntity();
+        userProfile.email=email;
+        userProfile.accessToken = id;
+        userProfile.displayName = displayName;
+        userProfile.photoUrl = photoUrl;
+
+        UserStore.to.saveProfile(userProfile);
+        var userbase = await db.collection("users").withConverter(
+            fromFirestore: UserData.fromFirestore,
+            toFirestore: (UserData userdata, options)=>userdata.toFirestore(),
+        ).where("id",isEqualTo :id).get();
+        if(userbase.docs.isEmpty){
+          final data = UserData(
+            id: id,
+            name: displayName,
+            email: email,
+            photourl: photoUrl,
+            location: "",
+            fcmtoken: "",
+            addtime: Timestamp.now()
+          );
+          await db.collection("users").withConverter(
+            fromFirestore: UserData.fromFirestore,
+            toFirestore: (UserData userdata, options)=>userdata.toFirestore(),
+          ).add(data);
+        }
+        toastInfo(msg: "Login success");
+        Get.offAndToNamed(AppRoutes.Application);
+      }
+    }catch(e){
+      toastInfo(msg: "Login error");
+      print(e);
+
+    }
+  }
+  @override
+  void onReady(){
+    super.onReady();
+    FirebaseAuth.instance.authStateChanges().listen((User? user){
+      if(user==null){
+        print("User is currently logged out");
+      }else{
+        print("User is currently logged in");
+      }
+    });
+  }
+
+}
